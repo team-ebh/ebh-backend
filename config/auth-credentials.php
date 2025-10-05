187ec798e2f98a4eeca0e9567551a31ae0686867
Morteza Babanezhad <m.babanezhad@nizek.com>
2025-10-05 18:23:27 +0330

    sprint 1: feat add admin credential

--- config/auth-credentials.php
+++ config/auth-credentials.php
@@ -5,12 +5,12 @@ declare(strict_types=1);
 return [
     'admin' => [
         'name' => "Tech's Admin",
-        'email' => env('ADMINISTRATOR_ACCOUNT_EMAIL', 'tech@nizek.com'),
-        'password' => env('ADMINISTRATOR_ACCOUNT_PASSWORD', 'N1ZEKezAd@Te!!%x%Z!$!#0'),
+        'email' => env('ADMINISTRATOR_ACCOUNT_EMAIL', 'tech-ebh@nizek.com'),
+        'password' => env('ADMINISTRATOR_ACCOUNT_PASSWORD', 'N1ZEKezAd@TEB!!%xH%Z!!W$#0'),
     ],
     'client' => [
         'name' => "Client's Admin",
-        'email' => env('CLIENT_ACCOUNT_EMAIL', 'client@admin.com'),
-        'password' => env('CLIENT_ACCOUNT_PASSWORD', 'N1ZekezAd@E!t%A%Z!$!#0'),
+        'email' => env('CLIENT_ACCOUNT_EMAIL', 'client-ebh@admin.com'),
+        'password' => env('CLIENT_ACCOUNT_PASSWORD', 'N1ZeKezAd@EB!t%H%Z!$!#0'),
     ],
 ];
