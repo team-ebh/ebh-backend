3867a4eb26b9ffb09633f8c3835cb540c220477f
Morteza Babanezhad <m.babanezhad@nizek.com>
2025-11-03 18:59:36 +0330

    EBH-4 feat: update socket web

--- routes/api/v1/test/socket.php
+++ routes/api/v1/test/socket.php
@@ -7,7 +7,16 @@ use App\Http\Controllers\Api\V1\TestSocketController;
 use Illuminate\Support\Facades\Route;
 
 // WebSocket test API endpoints (only available in non-risky environments)
-if (! ApplicationEnvironmentEnum::isRiskyEnvironment()) {
+if (!ApplicationEnvironmentEnum::isRiskyEnvironment()) {
     Route::get('connection-info', [TestSocketController::class, 'getConnectionInfo']);
     Route::post('send', [TestSocketController::class, 'sendMessage']);
+
+    // Handle CORS preflight request
+    Route::options('send', function () {
+        return response('', 204)
+            ->header('Access-Control-Allow-Origin', '*')
+            ->header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS')
+            ->header('Access-Control-Allow-Headers', 'Content-Type, Accept, Language, Authorization')
+            ->header('Access-Control-Max-Age', '86400');
+    });
 }
