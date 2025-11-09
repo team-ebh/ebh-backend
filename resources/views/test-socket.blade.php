33ae756638dad96b69b5933809947a1286decf4b
Morteza Babanezhad <m.babanezhad@nizek.com>
2025-11-09 16:53:31 +0330

    EBH-4 feat: dynamic force tls in socket test

--- resources/views/test-socket.blade.php
+++ resources/views/test-socket.blade.php
@@ -183,7 +183,6 @@
         wsPort: {{ config('broadcasting.connections.reverb.options.port') }},
         wssPort: {{ config('broadcasting.connections.reverb.options.port') }},
         forceTLS: {{ config('broadcasting.connections.reverb.options.scheme') === 'https' ? 'true' : 'false' }},
-        authEndpoint: 'https://api.dev.ebhapp.com/broadcasting/auth',
         enabledTransports: ['ws', 'wss'],
         cluster: 'mt1'
     });
