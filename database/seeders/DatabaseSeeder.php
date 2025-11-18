e7c873c703894127b3f8f7a334fad897b0c35bfe
Morteza Babanezhad <m.babanezhad@nizek.com>
2025-11-18 17:18:51 +0330

    EBH-5 feat: create test data seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,7 +13,8 @@ class DatabaseSeeder extends Seeder
     {
         $this->call([
             AdminInitializerSeeder::class,
-            //            VehicleSettingSeeder::class,
+            VehicleSettingSeeder::class,
+            TestDataSeeder::class,
         ]);
     }
 }
