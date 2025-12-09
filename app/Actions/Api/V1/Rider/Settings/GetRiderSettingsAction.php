243bbb31affe2c8089eb8478d619ebe5cf362dbc
Morteza Babanezhad <m.babanezhad@nizek.com>
2025-12-09 14:14:56 +0330

    EBH-6 feat: add new setting config in api

--- app/Actions/Api/V1/Rider/Settings/GetRiderSettingsAction.php
+++ app/Actions/Api/V1/Rider/Settings/GetRiderSettingsAction.php
@@ -14,12 +14,15 @@ readonly class GetRiderSettingsAction
     /**
      * Execute the action
      *
-     * @return array{trip_request_timeout_seconds: int}
+     * @return array{trip_request_timeout_seconds: int, rider_location_update_interval_seconds_online: int, rider_location_update_interval_seconds_busy: int, arriving_at_poll_interval_seconds: int}
      */
     public function __invoke(): array
     {
         return [
-            'trip_request_timeout_seconds' => 240,
+            'trip_request_timeout_seconds' => 45,
+            'rider_location_update_interval_seconds_online' => 15,
+            'rider_location_update_interval_seconds_busy' => 30,
+            'arriving_at_poll_interval_seconds' => 60,
         ];
     }
 }
